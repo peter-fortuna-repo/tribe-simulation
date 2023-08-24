997742fb8391beb916c4c354822dfa03120c3047
Peter Fortuna <peter.j.fortuna@gmail.com>
2023-08-24 16:00:25 -0400

    update

--- tribes.py
+++ tribes.py
@@ -1,22 +1,22 @@
-#religions.py
-# Caelan Osman, 
+# tribes.py
+# Peter Fortuna
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import odeint, solve_ivp, solve_bvp
 
 def define_ivp_system(n, C):
     '''
-    This returns a function defining the ODE system used in solve_ivp or odeint
-    :param n: ( n, ) np.ndarray difference between birth rate and death rate of the ith relgion
-    :param C: ( n, n ) np.ndarray matrix containing conversion rate where C[i,j]
-                       represents the conversion rate from j to i (diagonal is 0)
-    :param I: ( n, n ) np.ndarray matrix containing interactions between religions
-    :return: callable the ode system
+    This returns a function defining the differential equation (ode) system
+    :param n: ( n, ) np.ndarray difference between birth rate and death rate of the ith tribe
+    :param C: ( n, n ) np.ndarray matrix containing migration rate where C[i,j]
+                       represents the migration rate from j to i (diagonal is 0)
+    :param I: ( n, n ) np.ndarray matrix containing interactions between tribes
+    :return: a callable ode system
     '''
 
     def ode(t, P):
         '''
-        :param t: independent variable
+        :param t: independent variable (i.e. time)
         :param P: population change as a function of time
         :return: (np.ndarray) ode system
         '''
@@ -28,7 +28,7 @@ def define_ivp_system(n, C):
     return ode
 
 
-def numerical_solve(P0, t_span, M, n, C, method='ivp'):
+def numerical_solve(P0, t_span, M, n, C):
     '''
     Numerical solves the ivp using solve_ivp
     :parama P0: (n, ) np.ndarray array containing the initial value
@@ -36,20 +36,13 @@ def numerical_solve(P0, t_span, M, n, C, method='ivp'):
     :param disc: ( int ) temporal discretization
     :param n: (n, ) np.ndarray difference between birth rate and death rate of the ith relgion
     :param C: (n, n) np.ndarray matrix containing conversion rate differences
-    :param I: (n, n) np.ndarray matrix containing interactions between religions
+    :param I: (n, n) np.ndarray matrix containing interactions between tribes
     :param method ="ivp": boolean determing the method
     :return: callable the ode system
     '''
-    if method == 'ivp':
-        ode = define_ivp_system(n, C)
-        t_eval = np.linspace(t_span[0], t_span[-1], M)
-        sol =  solve_ivp(ode, t_span, P0, t_eval=t_eval)
-    elif method == 'odeint':
-        raise NotImplementedError( 'odeint not yet implemented' )
-    elif method == 'solve_bvp':
-        raise NotImplementedError( 'solve_bvp not yet implemented' )
-    else:
-        raise ValueError( 'incorrect method' )
+    ode = define_ivp_system(n, C)
+    t_eval = np.linspace(t_span[0], t_span[-1], M)
+    sol =  solve_ivp(ode, t_span, P0, t_eval=t_eval)
 
     return sol
 
@@ -66,8 +59,8 @@ def plot_sol(P0, t_span, M, n, C, method='ivp', labels=[]):
         ax.plot(sol.t, s, label=lab)
         ax.legend(loc='best')
 
-    ax.set_xlabel('Time')
+    ax.set_xlabel('Years')
     ax.set_ylabel('Population')
-    ax.set_title('SIR Model of religions')
+    ax.set_title('SIR Model of tribes')
     plt.show()
     return None
